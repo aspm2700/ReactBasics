a5b9f9673bc03ff4a57e6495d955c29ef2f58776
Ankit Mishra <137908698+aspm2700@users.noreply.github.com>
2024-03-29 17:01:31 +0000

    reactRouter

---
+++ 07reactRouter/src/components/Github/Github.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { useLoaderData } from 'react-router-dom'
+
+function Github() {
+    const data = useLoaderData()
+  return (
+    <>
+        <div className='text-center m-4 bg-gray-600 text-white p-4 text-3xl'>Github followers: {data.followers}
+            <div className=' flex justify-center items-center'>
+                <img className='mt-5' src={data.avatar_url} alt="Git picture" width = {300}/>
+            </div>
+        </div>
+    </>
+  )
+}
+
+
+
+export default Github
+
+
+export const githubInfoLoader = async() => {
+    let response = await fetch('https://api.github.com/users/aspm2700')
+    return response.json()
+}
