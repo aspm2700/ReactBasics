e41b441b7114c045e4f0bc59f33edd91b53baa95
Ankit Mishra <137908698+aspm2700@users.noreply.github.com>
2024-04-09 20:06:18 +0000

    reactreduxtoolkit

--- reduxToolkitTodo/src/components/AddTodo.jsx
+++ reduxToolkitTodo/src/components/AddTodo.jsx
@@ -1,10 +1,14 @@
 import {useDispatch,useSelector} from 'react-redux'
 import {addTodo,updateInput, updateTodo} from '../features/todo/todoSlice' 
-import React from 'react'
+import React, { useState } from 'react'
 
 function AddTodo() {
 
-    const {input,todoId} = useSelector(state=>state)
+    const todoId = useSelector(state=>state.todoId)
+
+    const inputData = useSelector(state=>state.input)
+
+    const [input, setInput] = useState('')
 
     const dispatch = useDispatch()
 
@@ -14,15 +18,13 @@ function AddTodo() {
           dispatch(addTodo(input))
         else
           dispatch(updateTodo({id: todoId, text: input}))
-        dispatch(updateInput(''))
         
+        dispatch(updateInput(''))
     }
 
-    const updateTodoHandler = (e) => {
-        dispatch(updateInput(e.target.value))
-    }
-
-     
+    React.useEffect(()=>{
+      setInput(inputData)
+    },[inputData])
 
         
     return (
@@ -32,7 +34,7 @@ function AddTodo() {
         className="bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
         placeholder="Enter a Todo..."
         value={input}
-        onChange={(e) => updateTodoHandler(e)}
+        onChange={(e) => setInput(e.target.value)}
       />
       
       <button
